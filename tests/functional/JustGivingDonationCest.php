58869a22b44a9d590965a079269c8d84fc92de0e
John Wheal <john@whealmedia.com>
2015-10-25 13:06:01 +0000

    Tested JustGiving donation details

--- tests/functional/JustGivingDonationCest.php
+++ tests/functional/JustGivingDonationCest.php
@@ -16,4 +16,18 @@ class JustGivingDonationCest
         $I->assertContains(env('JUST_GIVING_SHORT_URL'), $url);
         $I->assertContains('donated', $url);
     }
+
+    /**
+     * Test get JustGiving donation details
+     *
+     * @param FunctionalTester $I
+     */
+    public function testGetJustGivingDonationDetails(FunctionalTester $I)
+    {
+        //This donation should always be in JustGiving's database
+        $donationDetails = JustGiving::getDonationDetails('8628020');
+
+        $I->assertEquals('25.0000', $donationDetails->amount);
+        $I->assertEquals('Johnny Hume', $donationDetails->donorDisplayName);
+    }
 }
