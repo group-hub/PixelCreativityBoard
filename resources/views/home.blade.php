e5ab6ad600a79c646047f562a158849164db250d
John Wheal <john@whealmedia.com>
2015-11-06 13:24:08 +0000

    Added first set of info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,9 +15,81 @@
             </table>
         </div>
         <a href="{{ $justGivingUrl }}" class="donate-button">Donate</a>
+        <a href="{{ $justGivingUrl }}" class="instructions-button">Instructions</a>
 
+        <div class="page-container main-contents fundraiser-container">
+            <h2>University of Portsmouth Team 2015</h2>
+            <ul>
+                <li>
+                    <img src="https://scontent-lhr3-1.xx.fbcdn.net/hprofile-xta1/v/t1.0-1/p320x320/12116001_890728967641152_4429159945721636821_n.jpg?oh=577d5d19fd841f2c6e53638290da583b&oe=56F92A1F" alt="Name" />
+                    <p>Ming Wu</p>
+                </li>
+                <li>
+                    <img src="https://scontent-lhr3-1.xx.fbcdn.net/hprofile-xta1/v/t1.0-1/p320x320/12116001_890728967641152_4429159945721636821_n.jpg?oh=577d5d19fd841f2c6e53638290da583b&oe=56F92A1F" alt="Name" />
+                    <p>This persons name</p>
+                </li>
+                <li>
+                    <img src="https://scontent-lhr3-1.xx.fbcdn.net/hprofile-xta1/v/t1.0-1/p320x320/12116001_890728967641152_4429159945721636821_n.jpg?oh=577d5d19fd841f2c6e53638290da583b&oe=56F92A1F" alt="Name" />
+                    <p>This persons name</p>
+                </li>
+                <li>
+                    <img src="https://scontent-lhr3-1.xx.fbcdn.net/hprofile-xta1/v/t1.0-1/p320x320/12116001_890728967641152_4429159945721636821_n.jpg?oh=577d5d19fd841f2c6e53638290da583b&oe=56F92A1F" alt="Name" />
+                    <p>This persons name</p>
+                </li>
+                <li>
+                    <img src="https://scontent-lhr3-1.xx.fbcdn.net/hprofile-xta1/v/t1.0-1/p320x320/12116001_890728967641152_4429159945721636821_n.jpg?oh=577d5d19fd841f2c6e53638290da583b&oe=56F92A1F" alt="Name" />
+                    <p>This persons name</p>
+                </li>
+                <li>
+                    <img src="https://scontent-lhr3-1.xx.fbcdn.net/hprofile-xta1/v/t1.0-1/p320x320/12116001_890728967641152_4429159945721636821_n.jpg?oh=577d5d19fd841f2c6e53638290da583b&oe=56F92A1F" alt="Name" />
+                    <p>This persons name</p>
+                </li>
+                <li>
+                    <img src="https://scontent-lhr3-1.xx.fbcdn.net/hprofile-xta1/v/t1.0-1/p320x320/12116001_890728967641152_4429159945721636821_n.jpg?oh=577d5d19fd841f2c6e53638290da583b&oe=56F92A1F" alt="Name" />
+                    <p>This persons name</p>
+                </li>
+                <li>
+                    <img src="https://scontent-lhr3-1.xx.fbcdn.net/hprofile-xta1/v/t1.0-1/p320x320/12116001_890728967641152_4429159945721636821_n.jpg?oh=577d5d19fd841f2c6e53638290da583b&oe=56F92A1F" alt="Name" />
+                    <p>This persons name</p>
+                </li>
+            </ul>
+            <div class="clear-fix"></div>
+        </div>
         <div class="page-container main-contents">
+            <h2>What is this?</h2>
             <p>This will have information</p>
+        </div>
+        <div class="page-container main-contents impact-container">
+            <h2>East African Playgrounds impact so far</h2>
+            <ul>
+                <li>
+                    <img src="/images/impact/image3.jpg" alt="" />
+                    <p>131000</p>
+                    <h3>Children Impacted</h3>
+                </li>
+                <li>
+                    <img src="/images/impact/image1.jpg" alt="" />
+                    <p>85</p>
+                    <h3>Playgrounds</h3>
+                </li>
+                <li>
+                    <img src="/images/impact/image4.jpg" alt="" />
+                    <p>30</p>
+                    <h3>Arts and Play</h3>
+                </li>
+                <li>
+                    <img src="/images/impact/image2.jpg" alt="" />
+                    <p>25</p>
+                    <h3>Ugandans Trained</h3>
+                </li>
+            </ul>
+            <div class="clear-fix"></div>
+        </div>
+        <div class="page-container main-contents">
+            <h2>Team Donation Target</h2>
+            <div class="target">
+                <div class="amount-raised"></div>
+            </div>
         </div>
     </div>
 
--- resources/views/select.blade.php
+++ resources/views/select.blade.php
@@ -1,57 +1,59 @@
 @include ('partials/_head')
-<div class="container">
-    <h1>Selected <span id="selected-pixels">0</span> of <span id="max-pixels">{{ $maxPixels }}</span> pixels</h1>
-    <div class="grid">
-        <table>
-            @foreach($pixels as $gridRow)
-                <tr class="row-{{ $gridRow[0]->y }}">
-                    @foreach($gridRow as $pixel)
-                        <td id="{{ $pixel->x }}x{{ $pixel->y }}" @if ($pixel->color != null) style="background-color: {{ $pixel->color }}" class="disabled"@endif></td>
-                    @endforeach
-                </tr>
-            @endforeach
-        </table>
-    </div>
-    <div class="color-selector" style="display: none">
+    @include ('partials/_header')
+
+    <div class="container">
+        <h1>Selected <span id="selected-pixels">0</span> of <span id="max-pixels">{{ $maxPixels }}</span> pixels</h1>
+        <div class="grid">
+            <table>
+                @foreach($pixels as $gridRow)
+                    <tr class="row-{{ $gridRow[0]->y }}">
+                        @foreach($gridRow as $pixel)
+                            <td id="{{ $pixel->x }}x{{ $pixel->y }}" @if ($pixel->color != null) style="background-color: {{ $pixel->color }}" class="disabled"@endif></td>
+                        @endforeach
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+        <div class="color-selector" style="display: none">
+            <select>
+                <option value="#ac725e">#ac725e</option>
+                <option value="#d06b64">#d06b64</option>
+                <option value="#f83a22">#f83a22</option>
+                <option value="#fa573c">#fa573c</option>
+                <option value="#ff7537">#ff7537</option>
+                <option value="#ffad46">#ffad46</option>
+                <option value="#42d692">#42d692</option>
+                <option value="#16a765">#16a765</option>
+                <option value="#7bd148">#7bd148</option>
+                <option value="#b3dc6c">#b3dc6c</option>
+                <option value="#fbe983">#fbe983</option>
+                <option value="#fad165">#fad165</option>
+                <option value="#92e1c0">#92e1c0</option>
+                <option value="#9fe1e7">#9fe1e7</option>
+                <option value="#9fc6e7">#9fc6e7</option>
+                <option value="#4986e7">#4986e7</option>
+                <option value="#9a9cff">#9a9cff</option>
+                <option value="#b99aff">#b99aff</option>
+                <option value="#c2c2c2">#c2c2c2</option>
+                <option value="#cabdbf">#cabdbf</option>
+                <option value="#cca6ac">#cca6ac</option>
+                <option value="#f691b2">#f691b2</option>
+                <option value="#cd74e6">#cd74e6</option>
+                <option value="#a47ae2">#a47ae2</option>
+            </select>
+        </div>
         <select>
-            <option value="#ac725e">#ac725e</option>
-            <option value="#d06b64">#d06b64</option>
-            <option value="#f83a22">#f83a22</option>
-            <option value="#fa573c">#fa573c</option>
-            <option value="#ff7537">#ff7537</option>
-            <option value="#ffad46">#ffad46</option>
-            <option value="#42d692">#42d692</option>
-            <option value="#16a765">#16a765</option>
-            <option value="#7bd148">#7bd148</option>
-            <option value="#b3dc6c">#b3dc6c</option>
-            <option value="#fbe983">#fbe983</option>
-            <option value="#fad165">#fad165</option>
-            <option value="#92e1c0">#92e1c0</option>
-            <option value="#9fe1e7">#9fe1e7</option>
-            <option value="#9fc6e7">#9fc6e7</option>
-            <option value="#4986e7">#4986e7</option>
-            <option value="#9a9cff">#9a9cff</option>
-            <option value="#b99aff">#b99aff</option>
-            <option value="#c2c2c2">#c2c2c2</option>
-            <option value="#cabdbf">#cabdbf</option>
-            <option value="#cca6ac">#cca6ac</option>
-            <option value="#f691b2">#f691b2</option>
-            <option value="#cd74e6">#cd74e6</option>
-            <option value="#a47ae2">#a47ae2</option>
+            @foreach($fundraisers as $fundraiser)
+                <option value="{{ $fundraiser->id }}">{{ $fundraiser->name }}</option>
+            @endforeach
         </select>
+        <div class="save-button">Save</div>
     </div>
-    <select>
-        @foreach($fundraisers as $fundraiser)
-            <option value="{{ $fundraiser->id }}">{{ $fundraiser->name }}</option>
-        @endforeach
-    </select>
-    <div class="save-button">Save</div>
-</div>
-<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-<script>
-    var saveUrl = "{{ $siteUrl }}/save/{{ $donationId }}";
-    var siteUrl = "{{ $siteUrl }}";
-</script>
-<script src="/js/all.js"></script>
-</body>
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script>
+        var saveUrl = "{{ $siteUrl }}/save/{{ $donationId }}";
+        var siteUrl = "{{ $siteUrl }}";
+    </script>
+    <script src="/js/all.js"></script>
+    </body>
 </html>
