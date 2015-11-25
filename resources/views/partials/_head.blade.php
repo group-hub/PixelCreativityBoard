9f42278f0762051ba8eddc086637c8bbd8199616
John Wheal <john@whealmedia.com>
2015-11-25 22:33:53 +0000

    Added social sharing meta data

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -10,5 +10,27 @@
 
     <link rel="shortcut icon" href="/images/favicon.png">
 
+    <link rel="canonical" href="" />
+    <meta property="fb:app_id" content="1135780893118642" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Pixtivity, where creativity does good" />
+    <meta property="og:description" content="Pixtivity was made by University of Portsmouth students to fundraise for our volunteering trip to Uganda with East African Playgrounds." />
+    <meta property="og:url" content="http://pixtivity.org/" />
+    <meta property="og:site_name" content="Pixtivity" />
+    <meta property="og:image" content="{{ env('SITE_URL') }}/images/facebook.jpg" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta property="article:publisher" content="https://www.facebook.com/hellopixtivity" />
+
+    <!-- TWITTER META DATA -->
+    <meta property="twitter:account_id" content="4253838435" />
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@hellopixtivity">
+    <meta name="twitter:creator" content="@hellopixtivity">
+    <meta name="twitter:title" content="Pixtivity, where creativity does good">
+    <meta name="twitter:description" content="Pixtivity was made by University of Portsmouth students to fundraise for our volunteering trip to Uganda with East African Playgrounds.">
+    <meta name="twitter:image:src" content="{{ env('SITE_URL') }}/images/twitter.jpg">
+
 </head>
 <body>
