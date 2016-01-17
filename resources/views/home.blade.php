2ec815b2893ffdf79cd724c3d0ceddb32d02e784
John Wheal <john@whealmedia.com>
2016-01-17 01:27:16 +0000

    Added new homepage (without grid)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,27 @@
 
 <noscript>Pixtivity will not work without javascript</noscript>
 
+<div class="wrapper">
+    <div class="left animated fadeInLeft">
+        <div class="grid">
+
+        </div>
+        <div class="creators">
+            <p>Created by <a href="#">John Wheal</a>, <a href="#">Ming Wu</a> &amp; <a href="#">Hannah Storey</a></p>
+            <p>Supported by <a href="https://www.grouphub.co">Group Hub</a> &amp; <a href="http://www.uu-u.uk">UU-U</a></p>
+        </div>
+    </div>
+
+    <div class="right">
+        <!--<img src="/images/logo.svg" alt="logo" class="logo animated fadeInDown" />-->
+        <h2 class="animated fadeInDown">Help Build Playgrounds</h2>
+        <h3 class="animated fadeInDown"><span>Create and Donate</span></h3>
+        <a href="#" class="button start animated fadeInUp">Start</a>
+        <img src="/images/justgiving.png" alt="JustGiving" class="powered-by-just-giving animated fadeInUp" />
+        <div class="bottom animated fadeInUp">
+            <img src="/images/eap-logo.png" alt="East African Playgrounds" class="eap-logo" />
+        </div>
+    </div>
+</div>
 
 @include ('partials/_foot')
--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -4,8 +4,10 @@
     <title>Pixtivity</title>
     <meta name="description" content="Pixtivity was made by University of Portsmouth students to fundraise for our volunteering trip to Uganda with East African Playground. We are going there to help build a playground and help save silverback gorillas." />
 
+    <link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/animate.min.css">
     <link rel="stylesheet" href="/css/tooltipster.css">
 
     <link rel="shortcut icon" href="/images/favicon.png">
